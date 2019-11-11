0a73c11dd131c4cf5be2011083fe6b6a23f34d30
Abdelhamid Chirchi <abdelhamid.chirchi@be-softilys.tn>
2019-11-11 08:52:40 +0100

    first commit

---
+++ src/main/java/com/ncq/secondAlgo/Solution.java
@@ -0,0 +1,37 @@
+package com.ncq.secondAlgo;
+
+public class Solution {
+
+	public int[] solution(int N, int[] A) {
+
+		final int condition = N + 1;
+		int currentMax = 0;
+		int lastUpdate = 0;
+		int countersArray[] = new int[N];
+
+		for (int i = 0; i < A.length; i++) {
+			int currentValue = A[i];
+			if (currentValue == condition) {
+				lastUpdate = currentMax;
+			} else {
+				int position = currentValue - 1;
+				if (countersArray[position] < lastUpdate)
+					countersArray[position] = lastUpdate + 1;
+				else
+					countersArray[position]++;
+
+				if (countersArray[position] > currentMax) {
+					currentMax = countersArray[position];
+				}
+			}
+
+		}
+
+		for (int i = 0; i < N; i++)
+			if (countersArray[i] < lastUpdate)
+				countersArray[i] = lastUpdate;
+
+		return countersArray;
+	}
+
+}
